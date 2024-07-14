d1733a087fc1c88581bb2dcb212d809c08ecbbcd
AgnieszkaSurtel <164245101+AgnieszkaSurtel@users.noreply.github.com>
2024-07-14 20:09:19 +0200

    Add files via upload

---
+++ UzytkownikManager.h
@@ -0,0 +1,42 @@
+#ifndef UZYTKOWNIKMANAGER_H
+#define UZYTKOWNIKMANAGER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "Uzytkownik.h"
+#include "PlikZUzytkownikami.h"
+#include "MetodyPomocnicze.h"
+
+
+using namespace std;
+
+class UzytkownikManager
+{
+    int idZalogowanegoUzytkownika;
+    vector <Uzytkownik> uzytkownicy;
+    PlikZUzytkownikami plikZUzytkownikami;
+
+    Uzytkownik podajDaneNowegoUzytkownika ();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
+public:
+    UzytkownikManager(string nazwaPlikuZUzytkownikami) : plikZUzytkownikami (nazwaPlikuZUzytkownikami)
+    {
+        idZalogowanegoUzytkownika = 0;
+        uzytkownicy = plikZUzytkownikami.wczytajUzytkownikowZPliku();
+    }
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+    void logowanieUzytkownika();
+    void zmianaHaslaZalogowanegoUzytkownika();
+    void wylogowanieUzytkownika();
+    bool czyUzytkownikJestZalogowany();
+    int pobierzIdZalogowanegoUzytkownika();
+
+};
+#endif
