4ba9c1be49739e247192f3e6c77beba0450f752f
AgnieszkaSurtel <164245101+AgnieszkaSurtel@users.noreply.github.com>
2024-07-21 20:48:04 +0200

    Add files via upload

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,27 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+
+class MetodyPomocnicze
+{
+public:
+    MetodyPomocnicze();
+    static string konwersjaIntNaString(int liczba);
+    static string wczytajLinie();
+    static string zamienPierwszaLitereNaDuzaAPozostaleNaMale(string tekst);
+    static int konwersjaStringNaInt(string liczba);
+    static char wczytajZnak();
+    static int wczytajLiczbeCalkowita();
+    static void usunOdczytywanyPlik(string nazwaPlikuZRozszerzeniem);
+    static void zmienNazweTymczasowegoPlikuNaNazweOdczytywanegoPliku(string nazwaTymczasowegoPlikuZRozszerzeniem, string nazwaPlikuZRozszerzeniem);
+
+};
+
+#endif
