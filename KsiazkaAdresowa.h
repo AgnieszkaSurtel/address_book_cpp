b26baae6289dac141359da4f50b9b2999234121e
AgnieszkaSurtel <164245101+AgnieszkaSurtel@users.noreply.github.com>
2024-07-21 20:47:44 +0200

    Add files via upload

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,42 @@
+#ifndef KSIAZKAADRESOWA_H
+#define KSIAZKAADRESOWA_H
+
+#include <iostream>
+
+#include "UzytkownikManager.h"
+#include "AdresatManager.h"
+
+class KsiazkaAdresowa
+{
+
+    UzytkownikManager uzytkownikManager;
+    AdresatManager *adresatManager;
+    const string NAZWA_PLIKU_Z_ADRESATAMI;
+
+public:
+    KsiazkaAdresowa(string NAZWAPLIKUZUZYTKOWNIKAMI,string NAZWAPLIKUZADRESATAMI):
+        uzytkownikManager(NAZWAPLIKUZUZYTKOWNIKAMI),NAZWA_PLIKU_Z_ADRESATAMI(NAZWAPLIKUZADRESATAMI)
+    {
+        adresatManager = NULL;
+    }
+    ~KsiazkaAdresowa()
+    {
+        delete adresatManager;
+        adresatManager = NULL;
+    }
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+    void logowanieUzytkownika();
+    void zmianaHaslaZalogowanegoUzytkownika();
+    void wylogowanieUzytkownika();
+    void dodajAdresata();
+    void wyswietlWszystkichAdresatow();
+    void wczytajAdresatowZPliku();
+    bool czyUzytkownikJestZalogowany();
+    void wyszukajAdresatowPoImieniu();
+    void wyszukajAdresatowPoNazwisku();
+    void edytujAdresata();
+    int usunAdresata();
+};
+
+#endif
