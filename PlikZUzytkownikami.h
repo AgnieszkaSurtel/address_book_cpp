e0a84449696c57814413f474c5f24cdcc60e3dba
AgnieszkaSurtel <164245101+AgnieszkaSurtel@users.noreply.github.com>
2024-07-21 20:48:59 +0200

    Add files via upload

---
+++ PlikZUzytkownikami.h
@@ -0,0 +1,26 @@
+#ifndef PLIKZUZYTKOWNIKAMI_H
+#define PLIKZUZYTKOWNIKAMI_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+
+#include "Uzytkownik.h"
+#include "MetodyPomocnicze.h"
+#include "PlikTekstowy.h"
+
+class PlikZUzytkownikami :public PlikTekstowy
+{
+    const string NAZWA_PLIKU_Z_UZYTKOWNIKAMI;
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
+
+public:
+    PlikZUzytkownikami(string nazwaPlikuZUzytkownikami):NAZWA_PLIKU_Z_UZYTKOWNIKAMI(nazwaPlikuZUzytkownikami){}
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    vector <Uzytkownik> wczytajUzytkownikowZPliku();
+    void zapiszWszystkichUzytkownikowDoPliku(vector <Uzytkownik> &uzytkownicy);
+};
+
+#endif
